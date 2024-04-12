022175e4d15f4aeb603dc9585d60e1aa9c737c92
AdityaKulkarni <kulkarniaditya1997@gmail.com>
2024-04-12 11:03:21 -0700

    Removed main

--- src/index.ts
+++ src/index.ts
@@ -7,50 +7,42 @@ dotenv.config()
 
 const CLUSTER: Cluster = 'devnet'
 
-async function main() {
-	/**
-	 * Create a connection and initialize a keypair if one doesn't already exists.
-	 * If a keypair exists, airdrop a sol if needed.
-	 */
-	const connection = new Connection('http://127.0.0.1:8899')
+/**
+ * Create a connection and initialize a keypair if one doesn't already exists.
+ * If a keypair exists, airdrop a sol if needed.
+ */
+const connection = new Connection('http://127.0.0.1:8899')
 
-	const payer = await initializeKeypair(connection)
+const payer = await initializeKeypair(connection)
 
-	console.log(`public key: ${payer.publicKey.toBase58()}`)
+console.log(`public key: ${payer.publicKey.toBase58()}`)
 
-	const decimals = 0
-	const maxMembers = 3
+const decimals = 0
+const maxMembers = 3
 
-	const mintKeypair = Keypair.generate()
-	let mint = mintKeypair.publicKey
-	console.log(
-		'\nmint public key: ' + mintKeypair.publicKey.toBase58() + '\n\n'
-	)
+const mintKeypair = Keypair.generate()
+let mint = mintKeypair.publicKey
+console.log('\nmint public key: ' + mintKeypair.publicKey.toBase58() + '\n\n')
 
-	const metadata: TokenMetadata = {
-		name: 'cool-cats',
-		updateAuthority: payer.publicKey,
-		mint,
-		symbol: 'MEOW',
-		uri: 'https://solana.com/',
-		additionalMetadata: [
-			['species', 'Cat'],
-			['breed', 'Cool'],
-		],
-	}
-
-	const signature = await createGroup(
-		connection,
-		payer,
-		mintKeypair,
-		decimals,
-		maxMembers,
-		metadata
-	)
-
-	console.log(
-		`Created collection mint with metadata. Signature: ${signature}`
-	)
+const metadata: TokenMetadata = {
+	name: 'cool-cats',
+	updateAuthority: payer.publicKey,
+	mint,
+	symbol: 'MEOW',
+	uri: 'https://solana.com/',
+	additionalMetadata: [
+		['species', 'Cat'],
+		['breed', 'Cool'],
+	],
 }
 
-main()
+const signature = await createGroup(
+	connection,
+	payer,
+	mintKeypair,
+	decimals,
+	maxMembers,
+	metadata
+)
+
+console.log(`Created collection mint with metadata. Signature: ${signature}`)

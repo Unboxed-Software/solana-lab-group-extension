12b5bd659a65f8311c9e49656f47d5e268611aea
AdityaKulkarni <kulkarniaditya1997@gmail.com>
2024-04-12 11:23:31 -0700

    Changed connection cluster

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {initializeKeypair} from '@solana-developers/helpers'
-import {Cluster, Connection} from '@solana/web3.js'
+import {Cluster, Connection, clusterApiUrl} from '@solana/web3.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -9,7 +9,7 @@ const CLUSTER: Cluster = 'devnet'
  * Create a connection and initialize a keypair if one doesn't already exists.
  * If a keypair exists, airdrop a sol if needed.
  */
-const connection = new Connection('http://127.0.0.1:8899')
+const connection = new Connection(clusterApiUrl(CLUSTER))
 
 const payer = await initializeKeypair(connection)
 

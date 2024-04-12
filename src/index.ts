107244ebb47fe8f9352a9fcbbc288e0379dd731c
AdityaKulkarni <kulkarniaditya1997@gmail.com>
2024-04-12 11:22:13 -0700

    Starter code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import {initializeKeypair} from '@solana-developers/helpers'
-import {Cluster, Connection, Keypair} from '@solana/web3.js'
+import {Cluster, Connection} from '@solana/web3.js'
 import dotenv from 'dotenv'
-import {createGroup} from './create-mint'
-import {TokenMetadata} from '@solana/spl-token-metadata'
 dotenv.config()
 
 const CLUSTER: Cluster = 'devnet'
@@ -15,34 +13,4 @@ const connection = new Connection('http://127.0.0.1:8899')
 
 const payer = await initializeKeypair(connection)
 
-console.log(`public key: ${payer.publicKey.toBase58()}`)
-
-const decimals = 0
-const maxMembers = 3
-
-const mintKeypair = Keypair.generate()
-let mint = mintKeypair.publicKey
-console.log('\nmint public key: ' + mintKeypair.publicKey.toBase58() + '\n\n')
-
-const metadata: TokenMetadata = {
-	name: 'cool-cats',
-	updateAuthority: payer.publicKey,
-	mint,
-	symbol: 'MEOW',
-	uri: 'https://solana.com/',
-	additionalMetadata: [
-		['species', 'Cat'],
-		['breed', 'Cool'],
-	],
-}
-
-const signature = await createGroup(
-	connection,
-	payer,
-	mintKeypair,
-	decimals,
-	maxMembers,
-	metadata
-)
-
-console.log(`Created collection mint with metadata. Signature: ${signature}`)
+// CREATE GROUP MINT WITH METADATA
